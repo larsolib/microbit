62376e3687c0d7f085254244977687ca34d4e100
emilhThyri <emilThyri@gmail.com>
2018-04-04 14:29:46 +0200

    ferdig oppgave2

--- uart/main.c
+++ uart/main.c
@@ -1,19 +1,10 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "uart.h"
-#define GPIO ((NRF_GPIO_REGS*) 0x50000000)
+#include "gpio.h"
 
-typedef struct {
-	volatile uint32_t RESERVED0[321];
-	volatile uint32_t OUT;
-	volatile uint32_t OUTSET;
-	volatile uint32_t OUTCLR;
-	volatile uint32_t IN;
-	volatile uint32_t DIR;
-	volatile uint32_t DIRSET;
-	volatile uint32_t DIRCLR;
-	volatile uint32_t RESERVED1[120];
-	volatile uint32_t PIN_CNF[32];
-} NRF_GPIO_REGS;
+void illuminati_party();
+ssize_t _write(int fd, const void *buf, size_t count);
 
 int main(){
 	// Configure LED Matrix
@@ -30,19 +21,52 @@ int main(){
 	GPIO->PIN_CNF[26] = 0;
 
 	uart_init();
+	iprintf("Norway has %d counties.\n\r", 18);
 	int sleep = 0;
 	while(1){
+		char rx = uart_read();
 
-		if(!(GPIO->IN & (1 << 26))){
+		uart_send(rx);
+
+		if(!(GPIO->IN & (1 << 17))){
 			uart_send('A');
 		}
-		if(!(GPIO->IN & (1 << 17))){
+		if(!(GPIO->IN & (1 << 26))){
 			uart_send('B');
 		}
 
+		while((!(GPIO->IN & (1 << 17)))||(!(GPIO->IN & (1 << 26))));
 
-		sleep = 10000;
+		if(rx != ('\0')){illuminati_party();}
+
+		sleep = 1000;
 		while(--sleep);
 	}
 	return 0;
+}
+
+ssize_t _write(int fd, const void *buf, size_t count){
+	char * letter = (char *)(buf);
+	for(int i = 0; i < count; i++){
+		uart_send(*letter);
+		letter++;
+	}
+	return count;
+}
+
+void illuminati_party(){
+
+	static int TOGGLE = 1;
+
+	if(TOGGLE){
+		for(int row = 13; row < 16; row++){
+			GPIO->OUTSET = (1 << row);
+		}
+	}
+	else{	
+		for(int row = 13; row < 16; row++){
+			GPIO->OUTCLR = (1 << row);
+		}
+	}
+	TOGGLE = !TOGGLE;
 }
--- uart/uart.c
+++ uart/uart.c
@@ -14,54 +14,63 @@ typedef struct {
 	volatile uint32_t CTS;
 	volatile uint32_t NCTS;
 	volatile uint32_t RXDRDY;
-	volatile uint32_t RESERVED;
+	volatile uint32_t RESERVED[4];
 	volatile uint32_t TXDRDY;
-	volatile uint32_t RESERVED1[120];
+	volatile uint32_t RESERVED1[1];
+	volatile uint32_t ERROR;
+	volatile uint32_t RESERVED2[7];
+	volatile uint32_t RXTO;
+	volatile uint32_t RESERVED3[110];
 	volatile uint32_t INTEN;
 	volatile uint32_t INTENSET;
 	volatile uint32_t INTENCLR;
-	volatile uint32_t RESERVED2[93];
+	volatile uint32_t RESERVED4[93];
 	volatile uint32_t ERRORSRC;
-	volatile uint32_t RESERVED3[31];
+	volatile uint32_t RESERVED5[31];
 	volatile uint32_t ENABLE;
-	volatile uint32_t RESERVED4[1];
+	volatile uint32_t RESERVED6[1];
 	volatile uint32_t PSELRTS;
 	volatile uint32_t PSELTXD;
 	volatile uint32_t PSELCTS;
 	volatile uint32_t PSELRXD;
 	volatile uint32_t RXD;
 	volatile uint32_t TXD;
-	volatile uint32_t RESERVED5[1];
+	volatile uint32_t RESERVED7[1];
 	volatile uint32_t BAUDRATE;
-	volatile uint32_t RESERVED6[11];
+	volatile uint32_t RESERVED8[17];
 	volatile uint32_t CONFIG;
 } NRF_UART_REG;
 
 void uart_init(){
+	GPIO->PIN_CNF[24] = 1;
+	GPIO->PIN_CNF[25] = 0;
+
+	UART->PSELTXD = 24;
+	UART->PSELRXD = 25;
+
 	UART->PSELCTS = 0xFFFFFFFF;
 	UART->PSELRTS = 0xFFFFFFFF;
 	UART->BAUDRATE = 0x00275000;
 	
-
-	UART->PSELTXD = (1 << 24);
-	UART->PSELRXD = (1 << 25);
 	UART->ENABLE = 4;
 
 	UART->STARTTRX = 1;
 }
 
 void uart_send(char letter){
+	UART->STARTTX = 1;
 	UART->TXDRDY = 0;
-	UART->TXD = (uint32_t)letter;
-	while(UART->TXDRDY & (0)){continue;}
-	UART->STARTTRX = 0;
+	UART->TXD = letter;
+	while(!UART->TXDRDY);
+	UART->STOPTX = 1;
 }
 
 char uart_read(){
-	UART->RXDRDY = 0;
-	uint32_t temp = UART->RXD;
-	if(temp){
-		return (char)temp;
+	if(UART->RXDRDY){
+		UART->STARTTRX = 1;
+		char rx = UART->RXD;
+		UART->STOPRX = 1;
+		return rx;
 	}
 	return '\0';
 }
